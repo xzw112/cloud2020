2cb00f852f40331a7a0c7e3a45f7d0aa363f5b00
xzw <982078436@qq.com>
2020-05-25 23:13:42 +0800

    2020年5月25日23:13:02 架构重构

--- cloud-consumer-order80/src/main/java/com/xzw/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/xzw/springcloud/controller/OrderController.java
@@ -1,9 +1,29 @@
 package com.xzw.springcloud.controller;
 
+import com.xzw.springcloud.entities.CommonResult;
+import com.xzw.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 @Slf4j
 public class OrderController {
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping ("/consumer/payment/add")
+    public CommonResult<Payment> add(@RequestBody Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/add", payment, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") String id){
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
 }
