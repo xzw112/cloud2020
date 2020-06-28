59ef95e1866fdf1f3617b26c68dbe38c1c9c31a5
root <982078436@qq.com>
2020-06-28 17:37:20 +0800

    alibaba nacaos config

--- cloudalibaba-config-nacos-client3377/src/main/java/com/xzw/springcloud/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/xzw/springcloud/controller/ConfigClientController.java
@@ -1,12 +1,12 @@
 package com.xzw.springcloud.controller;
 
-import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Reference // 支持Nacos的动态刷新
+@RefreshScope // 支持Nacos的动态刷新
 public class ConfigClientController {
 
     @Value("${config.info}")
